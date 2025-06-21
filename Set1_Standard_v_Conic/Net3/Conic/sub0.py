ef28f5528375487b15324132985d3e05d02cff7b
Daniel <33268994+dblsBR@users.noreply.github.com>
2025-06-21 18:56:34 -0400

    Add files via upload

---
+++ Set1_Standard_v_Conic/Net3/Conic/sub0.py
@@ -0,0 +1,133 @@
+import networkx as nx;
+import gurobipy as gp;
+from gurobipy import GRB;
+import sys;
+import time;
+from datetime import datetime;
+import csv;
+import pandas as pd;
+import math;
+import random;
+import numpy;
+from numpy import *
+
+
+
+def sub0 (G, s, t, budget, gamma, cMin, cMax, k, timeLimit):
+    
+   
+    
+    print('\n--- Solving Sub 0 ---\n')
+       
+    
+    sub = gp.Model('sub0')
+    
+    obj = sub.addVar(vtype=GRB.INTEGER, lb = -GRB.INFINITY, ub = GRB.INFINITY, name = 'obj');
+    
+    c = sub.addVars(G.edges, vtype=GRB.INTEGER, lb = -GRB.INFINITY, ub = GRB.INFINITY, name = 'op_cost');
+    
+    y = sub.addVars(G.nodes, vtype=GRB.CONTINUOUS, lb = -GRB.INFINITY, ub = GRB.INFINITY, name='y');
+    w = sub.addVars(G.edges, vtype=GRB.CONTINUOUS, lb = -GRB.INFINITY, ub = 0, name='w');
+    p = sub.addVars(G.edges, vtype=GRB.CONTINUOUS, lb = -GRB.INFINITY, ub = 0, name='p');
+
+    
+    # Setting operational costs:   
+    random.seed(1);
+    
+    for i,j in G.edges:
+        if j == t:
+            if i == s:
+                sub.addConstr(c[i,j] == 0);
+            else:
+                center = random.randint(cMin, cMax);
+                # print('(%d,%d) range = [%d,%d]' %(i,j,center-5,center+5));
+                sub.addConstr(c[i,j] >= center-5);
+                sub.addConstr(c[i,j] <= center+5);
+        else:
+            sub.addConstr(c[i,j] == 0);
+    ###############################################################
+            
+        
+    
+
+
+    for i,j in G.edges:
+        sub.addConstr(p[i,j] >= 0);
+        
+        if G.edges[i,j]['special'] == 1:
+            sub.addConstr(y[i] - y[j] + w[i,j] <= 1);
+            
+        else:
+            sub.addConstr(y[i] - y[j] + w[i,j] <= 0);
+            
+    sub.addConstrs(gp.quicksum(p[i,j] for j in G.successors(i))- gp.quicksum(p[j,i] for j in G.predecessors(i)) == 0 for i in G.nodes);
+
+    # Integer Objective Value
+    sub.addConstr(gp.quicksum(G.nodes[i]['demand']*y[i] for i in G.nodes) 
+                                + gp.quicksum(G.edges[i,j]['capacity']*(1 - gamma[i,j])*w[i,j] for i,j in G.edges)
+                                + gp.quicksum(c[i,j]*p[i,j] for i,j in G.edges) == obj);
+    
+
+    sub.setObjective(gp.quicksum(G.nodes[i]['demand']*y[i] for i in G.nodes) 
+                                + gp.quicksum(G.edges[i,j]['capacity']*(1 - gamma[i,j])*w[i,j] for i,j in G.edges)
+                                + gp.quicksum(c[i,j]*p[i,j] for i,j in G.edges), GRB.MAXIMIZE);
+    
+    
+    
+    
+    sub.setParam("NonConvex", 2);
+    
+    sub.setParam("OutputFlag", 0);
+    sub.setParam("LogToConsole", 0);
+    sub.setParam("MIPFocus", 3);
+    sub.setParam("Cuts", 3);
+    sub.setParam("TimeLimit", timeLimit);
+    
+    sub.update();
+    sub.optimize();
+    
+        
+    UB = 10e8;
+    C = {};
+    for i,j in G.edges:
+        C[i,j] = 0;
+    
+    if sub.status == 3:
+        print('Infeasible!');
+        gap = 10e8;
+
+    
+    elif sub.status == 4:
+        print('Infeasible or Unbounded!');
+        gap = 10e8;
+
+        
+    elif sub.status == 2:
+        print('Solved to optimality!')
+        
+        for i,j in G.edges:
+            C[i,j] = c[i,j].x;
+            
+        UB = sub.ObjVal;
+        gap = 0;
+    elif sub.status == 9:
+        print('\nReached Time limt!\n')
+        
+        if sub.SolCount == 0:
+            gap = 10e8;
+            print('No feasible solution found!')
+            return None;
+
+        else:
+            print('MIP Gap %g\n' %sub.MIPGap);  
+        
+            for i,j in G.edges:
+                C[i,j] = c[i,j].x;
+            
+            UB = sub.ObjBound;
+            gap = sub.MIPGap;
+    else:
+        print('Model status: %d' %sub.status);
+      
+    
+    return C, UB, gap;
---
+++ Set1_Standard_v_Conic/Net3/Standard/submit_Test_Robust.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#SBATCH --job-name=Robust_Net3_Standard
+#SBATCH --nodes=1
+#SBATCH --tasks-per-node=1
+#SBATCH --mem=64gb
+#SBATCH --time=35:00:00
+#SBATCH --constraint=chip_type_6148g
+
+set -e
+
+module load anaconda3/2023.09-0 gurobi/10.0.1
+source activate gurobi_10
+
+srun python3 Test_sub_Standard.py
+
